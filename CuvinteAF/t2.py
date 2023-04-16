abec4bd1f8ffb32e4bec976c7546871259b8ac56
Tudor Chițu <115002435+tedi11@users.noreply.github.com>
2023-04-16 14:48:09 +0300

    Add files via upload

--- CuvinteAF/t2.py
+++ CuvinteAF/t2.py
@@ -1,11 +1,12 @@
 sol = []
+bucla = []
 lista_pasi = []
 def bkt(nod):
-    global sol, lista_pasi, lm
+    global sol, lista_pasi, lm, bucla
     #print("!")
     if len(lista_pasi) > lm:
         return
-    if destinations[nod] == 1:
+    if destinations[nod] == 1 and len(lista_pasi) == lm:
         #print("!")
         #sol.append(lista_pasi)
         print(*lista_pasi)
@@ -13,17 +14,30 @@ def bkt(nod):
     for i in range(n):
         #if i != nod:
         try:
-            for element in m[nod][i]:
+            for j in range(len(m[nod][i])):
+            #for element in m[nod][i]:
+                element = m[nod][i][j]
                 #print(lista_pasi)
-                lista_pasi.append(element)
-                #print(lista_pasi)
-                bkt(i)
-                lista_pasi.pop(len(lista_pasi)-1)  
+                if element == -1:
+                    continue
+                if element != 'x':
+                    #bucla = []
+                    lista_pasi.append(element)
+                    #print(lista_pasi)
+                    bkt(i)
+                    lista_pasi.pop(len(lista_pasi)-1)  
+                if element == 'x':
+                    #print("!")
+                    #if i in bucla:
+                    #    bucla = []
+                    #    break
+                    #bucla.append(i)
+                    m[nod][i][j] = -1
+                    bkt(i)
+                    m[nod][i][j] = 'x'
         except:
             pass   
     
-
-
 f = open("automat.txt")
 n = int(f.readline())
 lm = int(f.readline())
